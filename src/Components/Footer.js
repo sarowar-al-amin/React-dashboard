c4402c9bff118353fa065e743016bd64b04d112d
Sarowar Al Amin <30774817+sarowar-al-amin@users.noreply.github.com>
2020-12-07 14:40:31 +0600

    Footer, Menu and Content part of the dashboard

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className="main-footer">
+        <strong>Copyright ©</strong>
+        All rights reserved.
+        <div className="float-right d-none d-sm-inline-block">
+            <b>Version</b> 0.0.1
+        </div>
+        </footer>
+
+    )
+}
