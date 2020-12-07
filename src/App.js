148a8dd9f7007bc7db875aebdbe19e95061f3033
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2020-12-07 13:36:24 +0600

    Complete version 0.0.1

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ export default function App() {
     <div>
       <Header/>
       <Menu/>
-      <Footer/>
       <Content/>
+      <Footer/>
     </div>
   )
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>
-            Footer
+        <footer className="main-footer">
+        <strong>Copyright ©</strong>
+        All rights reserved.
+        <div className="float-right d-none d-sm-inline-block">
+            <b>Version</b> 0.0.1
         </div>
+        </footer>
+
     )
 }
